75943f626fcbffd7eada03e08eeadb35aeb2c95e
Dmitriy Orshanskiy <dmitriy_orshanskiy@MacBook-Air-Dmitriy.local>
2016-06-05 19:43:10 +0300

    deleted:    zbx/.rebar/erlcinfo 	deleted:    zbx/json/get_auth.json 	deleted:    zbx/json/get_hosts.json 	deleted:    zbx/json/get_items.json 	deleted:    zbx/src/.DS_Store 	deleted:    zbx/src/zbx.app.src 	deleted:    zbx/src/zbx_app.erl 	deleted:    zbx/src/zbx_config.hrl 	deleted:    zbx/src/zbx_connect.erl 	deleted:    zbx/src/zbx_sup.erl

--- zbx_connect/zbx/src/zbx_config.hrl
+++
@@ -1,9 +0,0 @@
--record(config, {
-    running_interval = 15000                                             :: integer(),
-    atsd_api_url = "http://<IP>:8088/api/v1/series/insert"         :: string(),
-    atsd_type = "application/json; charset=UTF-8"                        :: string(),
-    atsd_auth = [{"Authorization","Basic Y29sbGVjdG9yOmNvbGxlY3Rvcg=="}] :: list(),
-    zabbix_api_url = "http://<IP>/zabbix/api_jsonrpc.php"          :: string(),
-    zabbix_type = "application/json-rpc"                                 :: string(),
-    zabbix_auth = []                                                     :: list()
-}).
